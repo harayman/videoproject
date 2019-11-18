f86beb2f1c3d7c60b5edf9b6005f4b937a16e666
Haray <harayman@163.com>
2019-11-18 13:39:44 +0700

    递交项目

---
+++ mylibrary/src/main/java/com/example/mylibrary/Okhttp.java
@@ -0,0 +1,13 @@
+package com.example.mylibrary;
+
+import okhttp3.OkHttpClient;
+
+public class Okhttp {
+
+    static {
+        new OkHttpClient();
+    }
+
+
+
+}
---
+++ mylibrary/src/main/java/com/example/mylibrary/TestShow.java
@@ -0,0 +1,7 @@
+package com.example.mylibrary;
+
+public class TestShow {
+    public void test() {
+        System.out.println("测试信息，test()方法");
+    }
+}
